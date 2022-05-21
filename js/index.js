e326ba0313f72958b0d0788bb4a0168b6f7d7637
nanup <nanup.personal+github@gmail.com>
2022-05-21 16:07:57 +0530

    more updates

--- js/index.js
+++ js/index.js
@@ -1,31 +1,37 @@
-dieBattle();
+function checkFirstVisit() {
+  if (document.cookie.indexOf("true") == -1) {
+    document.cookie = "true";
+  } else {
+    dieBattle();
+  }
+}
 
 function dieBattle() {
-    var rollX = Math.floor(Math.random() * 6) + 1;
-    var rollY = Math.floor(Math.random() * 6) + 1;
+  var rollX = Math.floor(Math.random() * 6) + 1;
+  var rollY = Math.floor(Math.random() * 6) + 1;
 
-    var winner = findWinner(rollX, rollY);
+  var winner = findWinner(rollX, rollY);
 
-    rollDice(rollX, rollY)
+  rollDice(rollX, rollY);
 
-    if (winner == "draw") {
-        document.querySelector("h1").innerHTML = "It's a draw!";
-    } else {
-        document.querySelector("h1").innerHTML = winner + " won!";
-    }
+  if (winner == "draw") {
+    document.querySelector("h1").innerHTML = "It's a draw!";
+  } else {
+    document.querySelector("h1").innerHTML = winner + " won!";
+  }
 }
 
 function findWinner(x, y) {
-    if (x > y) {
-        return "Player X"
-    }
-    if (x < y) {
-        return "Player Y"
-    }
-    return "draw"
+  if (x > y) {
+    return "Player X";
+  }
+  if (x < y) {
+    return "Player Y";
+  }
+  return "draw";
 }
 
 function rollDice(x, y) {
-    document.querySelector(".die-x").src="images/dice-six-faces-" + x + ".png";
-    document.querySelector(".die-y").src="images/dice-six-faces-" + y + ".png";
+  document.querySelector(".die-x").src = "images/dice-six-faces-" + x + ".png";
+  document.querySelector(".die-y").src = "images/dice-six-faces-" + y + ".png";
 }
